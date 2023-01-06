45e8ab01f56c0a8012935b70dce3f517b826edc6
Thiago Vasconcelos <pedrosav14@gmail.com>
2023-01-06 17:01:03 -0300

    Localstorage used to save data and preventing too many requests

--- src/app/components/pokemon-card/pokemon-card.component.ts
+++ src/app/components/pokemon-card/pokemon-card.component.ts
@@ -10,16 +10,40 @@ export class PokemonCardComponent implements OnInit {
   @Input() pokemon: any;
   @Input() pokeinfo: any;
   spritesUrl: string = "";
+  id: number = 0;
+  pokemontype: any[] = [];
+  pktypes: any[] = [];
 
 
   constructor(private pokeapiService: PokeapiService){}
-  ngOnInit(){
-    this.pokeapiService.catchInfobyId(this.pokemon.url)
-    .subscribe((pokedata) => {
-      this.spritesUrl = pokedata.sprites['front_default']
-    })
+  
+  async ngOnInit() {
 
+    let data = localStorage.getItem(`${this.pokemon.name}: `);
+    if (data) {
 
+      let pokemonData = JSON.parse(data);
+      this.spritesUrl = pokemonData.spritesUrl;
+      this.id = pokemonData.id;
+      this.pktypes = pokemonData.pokeTypes
+    } else {
+
+      this.pokeapiService.catchInfobyId(this.pokemon.url)
+      .subscribe((pokedata) => {
+        this.spritesUrl = pokedata.sprites['front_default'];
+        this.id = pokedata.id;
+        this.pokemontype = pokedata.types
+        this.pktypes = this.pokemontype.map((types) => types['type'].name)
+
+        let pokemonData = { 
+          id: this.id,
+          spritesUrl: this.spritesUrl, 
+          pokeTypes: this.pktypes 
+        }
+
+        localStorage.setItem(`${this.pokemon.name}: `, JSON.stringify(pokemonData));
+      });
+    }
   }
 
 }
